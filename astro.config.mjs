6ffeef5f7f5f3ea7cc46ca9eacdfe636ac1160a2
Cristian <cristianvillalobos666@gmail.com>
2024-10-01 12:48:32 -0300

    awdaw

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fruna.netlify.app",
+  site: "https://fruna.teal.vercel.app",
   integrations: [tailwind()],
-	output: 'server',
-	adapter: netlify({ imageCDN: true }),
+	output: 'static',
 });
